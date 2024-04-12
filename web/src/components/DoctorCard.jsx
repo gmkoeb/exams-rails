358fe26617c90d13a712ebe4115780de5b5005a5
gmkoeb <gmkoeb@gmail.com>
2024-04-12 18:13:16 -0300

    Show doctor exams in doctor page
    Also included some css for navbar links

--- web/src/components/DoctorCard.jsx
+++ web/src/components/DoctorCard.jsx
@@ -5,12 +5,18 @@ export default function DoctorCard(props){
 
   async function fetchExams(id){
     const response = await fetch(`http://localhost:3000/api/v1/doctors/${id}/exams`);
-    const examList = await response.json();
-    setExams(examList);
+    let examList = await response.json();
+    if (exams.length == 0) {
+      examList = Object.groupBy(examList, ({ token }) => token)
+      const organizedList = Object.entries(examList)
+      setExams(organizedList)
+    }else{
+      setExams([])
+    }
   }
 
   return(
-    <div className="flex flex-col items-center mt-4 mx-auto border rounded-md w-1/2 bg-white">
+    <div className="flex flex-col items-center mt-4 mx-auto border rounded-md w-full bg-white">
       <h3 className="my-1 text-2xl border-b w-full text-center">{props.name}</h3>
       <div className="flex gap-10 mt-5">
         <div className="mb-5">
@@ -22,10 +28,32 @@ export default function DoctorCard(props){
           <p>{props.crm} - {props.crm_state}</p>
         </div>
       </div>
-      <button onClick={() => fetchExams(props.id)} className="border-t w-full">
-        <h3 className="text-2xl text-center">Exames</h3>
-        {exams.map(exam => exam.token)}
+      <button onClick={() => fetchExams(props.id)} className="border-t border-b w-full">
+        <h3 className="text-2xl text-center text-blue-500">Exames</h3>
       </button>
+      <div>
+        {exams.map(exam =>
+        <div className="text-center" key={exam[0]}> 
+          <h3 className="text-xl">{exam[0]}</h3>
+          <div>
+            {exam[1].map(examDetails => 
+            <div key={examDetails.id}>
+              <p>{new Date(examDetails.date).toString()}</p>
+              <div>
+                <h3 className="text-xl">Tipo do Exame</h3>
+                <p>{examDetails.exam_type}</p>
+              </div>
+              <div className="border-b-2 w-full mb-2">
+                <h3 className="text-xl">Limites e Resultado</h3>
+                <p>{examDetails.type_limits}</p>
+                <p>{examDetails.type_result}</p>
+              </div>
+            </div>
+            )}
+          </div>
+        </div>
+        )}
+      </div>
     </div>
   )
 }
--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header(){
+  function activeLinkColor(link) {
+    const location = useLocation().pathname;
+    if (location === link){
+      return 'active'
+    }else{
+      return ''
+    }
+  }
   return(
     <header className="flex bg-emerald-500 p-4">
       <Link to={'/'} className="text-3xl text-green-100">DoctorCare</Link>
-      <nav className="flex items-center ml-24 gap-5">
-        <Link to={'/doctors'}>Doctors</Link>
-        <Link to={'/patients'}>Patients</Link>
+      <nav className="text-xl fw-bold flex items-center ml-24 gap-5">
+        <Link className={`${activeLinkColor('/doctors')} hover:text-white`} to={'/doctors'}>Doctors</Link>
+        <Link className={`${activeLinkColor('/patients')} hover:text-white`} to={'/patients'}>Patients</Link>
       </nav>
     </header>
   )
